22476c21b323291860098c9fe849386c15776c53
chrisd08 <info@vitalic.co.uk>
2022-03-15 12:05:51 +0000

    reproduce value/compose bug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import otherStyles from "./otherStyles.css";
 import styles from "./style.css";
 
 console.log(styles);
 
 const element = document.querySelector(".element");
 
-element.innerHTML = `<div class="${styles.page}">
+element.innerHTML = `<div class="${styles.page} ${otherStyles.page}">
      <p class="${styles.text}">CSS Modules Webpack</p>
    </div>`;
--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,9 @@ module.exports = {
             loader: "css-loader",
             options: {
               importLoaders: 1,
-              modules: true,
+              modules: {
+                localIdentName: "[name]__[local]__[hash:base64:5]",
+              },
             },
           },
         ],
